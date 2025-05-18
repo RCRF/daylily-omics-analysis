58aa5373be77deba8305c7c2f0b06267acf7fce6
John Major <daylily@daylilyinformatics.com>
2025-05-17 23:21:05 -0700

    0.7.204

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="daylily-omics-analysis",
-    version="0.7.202",
+    version="0.7.204",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here
